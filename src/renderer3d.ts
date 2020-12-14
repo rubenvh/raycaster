aa98438c0bf592ce81843df97952680f0e002fb6
Ruben Van Havermaet <ruben.van.havermaet@gmail.com>
2020-12-14 21:30:33 +0100

    draw shadows on textures

--- src/renderer3d.ts
+++ src/renderer3d.ts
@@ -25,6 +25,7 @@ export class Renderer3d {
     private width: number;
     private height: number;
     private textures: HTMLCanvasElement;
+    textureContext: CanvasRenderingContext2D;
 
     constructor(private world: World, private canvas: HTMLCanvasElement) {
         this.context = canvas.getContext('2d');
@@ -35,12 +36,10 @@ export class Renderer3d {
         const imgUrl = 'https://filecache.garrysmods.org/2373/2/1024x768.jpg';
         this.textures = document.createElement('canvas') as HTMLCanvasElement;
 
-        const textureContext = this.textures.getContext('2d');
+        this.textureContext = this.textures.getContext('2d');
         this.textures.width = 1024; this.textures.height= 768;
         const img = new Image();
-        img.onload = function() {            
-            textureContext.drawImage(img, 0, 0);
-        }
+        img.onload = () => this.textureContext.drawImage(img, 0, 0);
         img.src = imgUrl;
 
 
@@ -131,7 +130,7 @@ export class Renderer3d {
 
     private drawWall = (wallProps: WallProps[]) => {        
         const start = wallProps[wallProps.length-1];
-        const end = wallProps[0];
+        const end = wallProps[0];        
         if (start.height <= 0) return;
         if (!start.material?.texture) {
             this.context.fillStyle = determineColor(wallProps[0].material);
@@ -146,7 +145,7 @@ export class Renderer3d {
             const tileFactor = 20 // => w.length for stretching
             const twidth = 192;
             const theight = 192;
-            const textureIndex = 1;
+            const textureIndex = 1;            
             for(let windex=wallProps.length-1; windex >= 0;windex--) {
                 const w = wallProps[windex];
                 const wx = distance(w.origin, w.intersection);
@@ -154,7 +153,15 @@ export class Renderer3d {
                 const [y1, y2] = wallProps[windex].rowRange;                
                 const tx = Math.floor((twidth * wx / tileFactor) % (twidth - 1));
                 this.context.drawImage(this.textures, tx+textureIndex*twidth, 0, 1, theight, x1, y1, x2-x1, y2-y1);                
-            }          
+            }                   
+            this.context.fillStyle = `rgba(0,0,0,${1-start.material.luminosity}`;
+            this.context.beginPath();
+            this.context.moveTo(start.colRange[1], start.rowRange[0]);
+            this.context.lineTo(start.colRange[1], start.rowRange[1]);
+            this.context.lineTo(end.colRange[0], end.rowRange[1]);
+            this.context.lineTo(end.colRange[0], end.rowRange[0]);
+            this.context.closePath();
+            this.context.fill();
         }
         
         // // draw selection borders if edge was selected
@@ -163,22 +170,24 @@ export class Renderer3d {
         //      drawRect(this.context, [[col1, endRow], [col2, endRow+2]], 'rgb(250,100,0)');
         // }        
     };
+
+    //private drawSingleColorWall = 
 }
 
 const determineColor = (material: IMaterial): string => {    
     if (material?.color) {        
         const color = material.color;
-        return `rgba(${color[0]/255 * material.luminosity},${color[1]/255 * material.luminosity},${color[2]/255 * material.luminosity},${color[3]})`;
+        return `rgba(${color[0] * material.luminosity},${color[1] * material.luminosity},${color[2] * material.luminosity},${color[3]})`;
     }
-    return `rgb(0,0,${material.luminosity})`;
+    return `rgb(0,0,${material.luminosity*155 + 100})`;
 };
 
 export const determineLight = (hit: RayHit) => {
-    let color = 100;
+    let color = 0.5;
     if (hit?.edge) {
         let m = Math.abs(slope([hit.edge.start.vector, hit.edge.end.vector]));            
-        if (!isFinite(m)) return 255;        
-        color += (m / (1 + m)) * (255-color);
+        if (!isFinite(m)) return 1;        
+        color += (m / (1 + m)) * (1 - color);
     }
     return color;
 };
