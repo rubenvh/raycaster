b6b441117cb91a913ea2a17a41f0b06f3cba2a31
Ruben Van Havermaet <ruben.van.havermaet@gmail.com>
2020-12-15 08:04:42 +0100

    fix test

--- src/renderer3d.test.ts
+++ src/renderer3d.test.ts
@@ -5,9 +5,9 @@ import { makeEdge } from './geometry/edge';
 describe('renderer3d tests', () => {
     describe('light intensity tests', () => {  
         const test = (e: [Vector, Vector]) => determineLight({edge: makeEdge(...e), distance: 0, intersection: null, polygon: null, ray: null});
-        it('horizontal edge => 100', () => expect(test([[0,0],[1,0]])).toBe(100));
-        it('vertical edge => 255', () => expect(test([[0,0],[0,1]])).toBe(255));
-        it('edge with slope 1 => ', () => expect(test([[0,0],[1,1]])).toBe(100+(255-100)/2));
-        it('edge with slope -1', () => expect(test([[0,1],[1,0]])).toBe(100+(255-100)/2));
+        it('horizontal edge => 0.5', () => expect(test([[0,0],[1,0]])).toBe(0.5));
+        it('vertical edge => 1', () => expect(test([[0,0],[0,1]])).toBe(1));
+        it('edge with slope 1 => ', () => expect(test([[0,0],[1,1]])).toBe(0.75));
+        it('edge with slope -1', () => expect(test([[0,1],[1,0]])).toBe(0.75));
     });
 });
