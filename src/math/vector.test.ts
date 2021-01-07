4cd0202bc805c5a9ca64a6f8f3af6cba85679cf9
Ruben Van Havermaet <ruben.van.havermaet@gmail.com>
2021-01-07 20:34:03 +0100

    adding vector tests for normSqr and normalize

--- src/math/vector.test.ts
+++ src/math/vector.test.ts
@@ -1,24 +1,11 @@
-import { add, angleBetween, areOrthogonal, dim, distance, dot, getX, getY, norm, perpendicular, proj, rotate, scale, subtract, Vector } from "./vector";
+import { add, angleBetween, areOrthogonal, distance, dot, getX, getY, norm, normalize, normSqr, perpendicular, proj, rotate, scale, subtract, Vector } from "./vector";
 import math = require("mathjs");
 
-describe('vector tests', () => {
-  describe('dimension tests', () => {    
-    it('two dimensional vertices', () => expect(dim([1, 1])).toBe(2));
-  });
-
+describe('vector tests', () => {  
   describe('component getter tests', () => {
     it('x component', () => expect(getX([1, 2])).toBe(1));
     it('y component', () => expect(getY([1, 2])).toBe(2));
-  });
-
-  describe('norm tests', () => {
-    it('||(0,1)|| = 1', () =>  expect(norm([ 0, 1])).toBe(1));
-    it('||(1,0)|| = 1', () =>  expect(norm([ 1, 0])).toBe(1));
-    it('||(0,-2)|| = 2', () => expect(norm([ 0,-2])).toBe(2));
-    it('||(-2,0)|| = 2', () => expect(norm([-2, 0])).toBe(2));
-    it('||(3,4)|| = 5', () =>  expect(norm([ 3, 4])).toBe(5));
-  });
-
+  });  
   describe('dot product tests', () => {
     const test = (x: Vector, y: Vector, e: number) => expect(dot(x, y)).toEqual(e);    
     it('(0,0).(1,1) = 0',  () => test([0,0], [1,1], 0));
@@ -26,7 +13,33 @@ describe('vector tests', () => {
     it('(1,1).(1,1) = 2',  () => test([1,1], [1,1], 2));
     it('(1,2).(3,4) = 11', () => test([1,2], [3,4], 11));
   });
-
+  describe('norm tests', () => {
+    it('||(0,1)|| = 1', () =>  expect(norm([ 0, 1])).toBe(1));
+    it('||(1,0)|| = 1', () =>  expect(norm([ 1, 0])).toBe(1));
+    it('||(0,-2)|| = 2', () => expect(norm([ 0,-2])).toBe(2));
+    it('||(-2,0)|| = 2', () => expect(norm([-2, 0])).toBe(2));
+    it('||(3,4)|| = 5', () =>  expect(norm([ 3, 4])).toBe(5));
+  });
+  describe('normSqr tests', () => {
+    it('(0,0) => 0', () =>  expect(normSqr([ 0, 0])).toBe(0));
+    it('(0,1) => 1', () =>  expect(normSqr([ 0, 1])).toBe(1));
+    it('(1,1) => 2', () =>  expect(normSqr([ 1, 1])).toBe(2));    
+    it('(2,1) => 5', () =>  expect(normSqr([ 2, 1])).toBe(5));    
+    it('(2,3) => 13', () =>  expect(normSqr([ 2, 3])).toBe(13));    
+    it('(2,-3) => 13', () =>  expect(normSqr([ 2, -3])).toBe(13));    
+    it('(-2,-3) => 13', () =>  expect(normSqr([ -2, -3])).toBe(13));    
+  });
+  describe('normalize tests: A / ||A||', () => {
+    it('( 0,  0) => (NaN, NaN)', () =>  expect(normalize([ 0, 0])).toEqual([NaN, NaN]));    
+    it('( 1,  0) => (1, 0)', () =>  expect(normalize([ 0, 1])).toEqual([0, 1]));    
+    it('( 0,  1) => (0, 1)', () =>  expect(normalize([ 1, 0])).toEqual([1, 0]));    
+    it('(10,  0) => (1, 0)', () =>  expect(normalize([ 0, 10])).toEqual([0, 1]));    
+    it('( 0, 10) => (0, 1)', () =>  expect(normalize([ 10, 0])).toEqual([1, 0]));    
+    it('( 1,  1) => (1/sqrt(2), 1/sqrt(2))', () =>  expect(normalize([   1,  1])).toEqual([ 1/Math.SQRT2,  1/Math.SQRT2]));    
+    it('(-1,  1) => (-1/sqrt(2), 1/sqrt(2))', () =>  expect(normalize([ -1,  1])).toEqual([-1/Math.SQRT2,  1/Math.SQRT2]));    
+    it('( 1, -1) => (1/sqrt(2), -1/sqrt(2))', () =>  expect(normalize([  1, -1])).toEqual([ 1/Math.SQRT2, -1/Math.SQRT2]));    
+    it('(-1, -1) => (-1/sqrt(2), -1/sqrt(2))', () =>  expect(normalize([-1, -1])).toEqual([-1/Math.SQRT2, -1/Math.SQRT2]));    
+  });
   describe('substract tests', () => {
     const test = (x: Vector, y: Vector, e: Vector) => expect(subtract(x, y)).toEqual(e);
     it('1 - 0 = 1',  () => test([1,1], [0,0], [ 1, 1]));
