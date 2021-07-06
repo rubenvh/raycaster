9521e5734e2a4bfa322e5a2d6d09e586fd93feeb
Ruben <ruben.van.havermaet@gmail.com>
2021-07-06 09:19:23 +0200

    draw BSP planes correctly when scrolling/zooming

--- src/common/drawing/drawing.ts
+++ src/common/drawing/drawing.ts
@@ -5,7 +5,7 @@ import { Vector } from "../math/vector";
 
 export const drawVector = (context: CanvasRenderingContext2D, vector: Vector, color: string = 'rgb(100,100,0)') => {
     context.beginPath();
-    context.arc(vector[0], vector[1], 2, 0, 2*Math.PI, false);
+    context.arc(vector[0], vector[1], 2, 0, 2 * Math.PI, false);
     context.fillStyle = color;
     context.fill();
 }
@@ -20,66 +20,66 @@ export const drawSegment = (context: CanvasRenderingContext2D, segment: ILineSeg
     context.stroke();
 };
 
-export const drawRect = (context: CanvasRenderingContext2D, segment: ILineSegment, color: string|CanvasGradient = 'rgb(0,0,255)') => {
+export const drawRect = (context: CanvasRenderingContext2D, segment: ILineSegment, color: string | CanvasGradient = 'rgb(0,0,255)') => {
     const x1 = segment[0][0],
-          y1 = segment[0][1],
-          x2 = segment[1][0],
-          y2 = segment[1][1];
-    context.fillStyle = color;    
-    context.fillRect(x1, y1, x2-x1, y2-y1);
-};   
+        y1 = segment[0][1],
+        x2 = segment[1][0],
+        y2 = segment[1][1];
+    context.fillStyle = color;
+    context.fillRect(x1, y1, x2 - x1, y2 - y1);
+};
 
 export const drawBoundingBox = (context: CanvasRenderingContext2D, boundingBox: BoundingBox, color: string = 'rgb(150,100,50,0.8)') => {
-    const x1 = boundingBox[0][0]-5,
-          y1 = boundingBox[0][1]-5,
-          x2 = boundingBox[1][0]+5,
-          y2 = boundingBox[1][1]+5;    
-    context.strokeStyle = color;        
+    const x1 = boundingBox[0][0] - 5,
+        y1 = boundingBox[0][1] - 5,
+        x2 = boundingBox[1][0] + 5,
+        y2 = boundingBox[1][1] + 5;
+    context.strokeStyle = color;
     context.lineWidth = 1;
     context.setLineDash([4, 2]);
-    context.strokeRect(x1, y1, x2-x1, y2-y1);
+    context.strokeRect(x1, y1, x2 - x1, y2 - y1);
 }
 
-export const drawTrapezoid = (context: CanvasRenderingContext2D, p: [Vector, Vector, Vector, Vector], color: string|CanvasGradient): void => {    
-    context.beginPath();    
+export const drawTrapezoid = (context: CanvasRenderingContext2D, p: [Vector, Vector, Vector, Vector], color: string | CanvasGradient): void => {
+    context.beginPath();
     context.moveTo(p[0][0], p[0][1]);
     context.lineTo(p[1][0], p[1][1]);
     context.lineTo(p[2][0], p[2][1]);
     context.lineTo(p[3][0], p[3][1]);
-    context.closePath();    
+    context.closePath();
     context.fillStyle = color;
     context.fill();
 };
 
-export const drawPlane = (context: CanvasRenderingContext2D, plane: Plane, color: string='green', clipRegion: Path2D[]): [Path2D, Path2D] => {
+export const drawPlane = (context: CanvasRenderingContext2D, plane: Plane, color: string = 'green', clipRegion: Path2D[], bounds: Vector): [Path2D, Path2D] => {
 
     context.save();
     for (const p of clipRegion) { context.clip(p); }
 
-    context.beginPath();    
-	context.strokeStyle = color;
-	context.lineWidth = 2;
+    context.beginPath();
+    context.strokeStyle = color;
+    context.lineWidth = 2;
 
-    const [xMax, yMax] = [context.canvas.width, context.canvas.height];
-    const [[x1, y1],[x2, y2]] = getBounds(plane, xMax, yMax);
+    const [xMax, yMax] = bounds;
+    const [[x1, y1], [x2, y2]] = getBounds(plane, xMax, yMax);
     context.moveTo(x1, y1);
-    context.lineTo(x2, y2);    
-	context.stroke();    
-    context.closePath();    
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
 
 
     context.restore();
-    
+
     if (isPlaneSemiHorizontal(plane)) {
-        const [up, down] = [                        
-            createClipTrapezoid([0,0],[x1,y1],[x2,y2],[xMax, 0]),
-            createClipTrapezoid([0,yMax], [x1,y1],[x2,y2],[xMax,yMax]),
+        const [up, down] = [
+            createClipTrapezoid([0, 0], [x1, y1], [x2, y2], [xMax, 0]),
+            createClipTrapezoid([0, yMax], [x1, y1], [x2, y2], [xMax, yMax]),
         ];
         return plane.n[0] < 0 ? [up, down] : [down, up];
-    } else {        
-        const [left, right] = [                        
-            createClipTrapezoid([0,0],[x1,y1],[x2,y2],[0,yMax]),
-            createClipTrapezoid([xMax,0], [x1,y1],[x2,y2],[xMax,yMax]),
+    } else {
+        const [left, right] = [
+            createClipTrapezoid([0, 0], [x1, y1], [x2, y2], [0, yMax]),
+            createClipTrapezoid([xMax, 0], [x1, y1], [x2, y2], [xMax, yMax]),
         ];
         return plane.n[0] < 0 ? [left, right] : [right, left];
     }
@@ -91,6 +91,6 @@ const createClipTrapezoid = (...p: [Vector, Vector, Vector, Vector]) => {
     result.lineTo(p[1][0], p[1][1]);
     result.lineTo(p[2][0], p[2][1]);
     result.lineTo(p[3][0], p[3][1]);
-    result.closePath();    
+    result.closePath();
     return result;
 }
--- src/common/editor/mapEditor.ts
+++ src/common/editor/mapEditor.ts
@@ -13,7 +13,7 @@ import { CastingStats, EMPTY_STATS } from '../raycaster';
 import { IUIConfigState } from '../store/ui-config';
 import { ISpaceTranslator } from '../actions/geometrySelector';
 import { Vector } from '../math/vector';
-import {ViewPort} from './viewport';
+import { ViewPort } from './viewport';
 
 export class MapEditorRenderer implements ISpaceTranslator {
 
@@ -21,70 +21,70 @@ export class MapEditorRenderer implements ISpaceTranslator {
     private selectedElements: SelectableElement[] = [];
     private camera = DEFAULT_CAMERA;
     private wallGeometry = EMPTY_GEOMETRY;
-    private selectedTreeNode: SelectableElement;    
+    private selectedTreeNode: SelectableElement;
     private castingStats: CastingStats = EMPTY_STATS;
     private uiConfig: IUIConfigState = {};
     private scroll: ViewPort;
-    
+
     elemLeft: number;
     elemTop: number;
     get context(): CanvasRenderingContext2D {
         return this._context;
     }
-    
+
     constructor(private canvas: HTMLCanvasElement) {
         this._context = canvas.getContext('2d');
         this.scroll = new ViewPort(canvas);
-        this.resizeCanvas();        
+        this.resizeCanvas();
         this.elemLeft = canvas.offsetLeft + canvas.clientLeft;
         this.elemTop = canvas.offsetTop + canvas.clientTop;
 
         window.addEventListener('resize', e => {
-            e.preventDefault();              
-            this.resizeCanvas();            
+            e.preventDefault();
+            this.resizeCanvas();
         });
-        
+
         connect(s => {
             this.selectedElements = s.selection.elements;
             this.selectedTreeNode = s.selection.treeSelection;
-            this.camera = s.player.camera;            
-            this.uiConfig = s.uiConfig;            
-            this.castingStats = s.stats.intersections.stats;            
+            this.camera = s.player.camera;
+            this.uiConfig = s.uiConfig;
+            this.castingStats = s.stats.intersections.stats;
 
             if (s.walls.geometry !== this.wallGeometry) {
                 this.wallGeometry = s.walls.geometry;
                 this.scroll.setBounds(this.wallGeometry.bounds);
-            }            
+            }
         });
     }
-   
-    public toWorldSpace = (event: MouseEvent): Vector => this.scroll.toWorldSpace([event.pageX-this.elemLeft, event.pageY - this.elemTop]);
+
+    public toWorldSpace = (event: MouseEvent): Vector => this.scroll.toWorldSpace([event.pageX - this.elemLeft, event.pageY - this.elemTop]);
 
     private get active() {
         return !this.uiConfig.enableTestCanvas;
     }
 
-    private resizeCanvas = (): void => {        
-        this.canvas.height = 0;        
+    private resizeCanvas = (): void => {
+        this.canvas.height = 0;
         this.canvas.width = this.canvas.clientWidth;
         this.canvas.height = this.canvas.parentElement.clientHeight;
-        this.scroll.reset();       
+        this.scroll.reset();
     }
 
     public render = (fps: number) => {
-        if (this.active) {            
+        if (this.active) {
             this.scroll.drawBackground();
 
             this.drawCamera(this._context, this.camera);
             this.drawGeometry(this._context, this.wallGeometry);
 
-            if (this.wallGeometry.bsp && this.uiConfig.drawBsp) {                
+            if (this.wallGeometry.bsp && this.uiConfig.drawBsp) {
                 this.drawBsp(this.wallGeometry.bsp);
             }
 
             this.scroll.drawForeground();
-        }       
-                
+        }
+
         // if (this.world.rays?.length > 0) {
         //     this.world.rays.forEach((c, rayIndex) => {
         //         // c.hits.forEach(hit => {
@@ -104,44 +104,45 @@ export class MapEditorRenderer implements ISpaceTranslator {
         });
     };
 
-    private drawGeometry = (context: CanvasRenderingContext2D, geometry: IGeometry) => {        
-        geometry.polygons.forEach(p => {            
+    private drawGeometry = (context: CanvasRenderingContext2D, geometry: IGeometry) => {
+        geometry.polygons.forEach(p => {
             const tested = this.uiConfig.drawBsp && this.castingStats.polygons.has(p.id);
-            const selected = isSelectedPolygon(p.id, this.selectedElements);            
-            const highlighted = this.selectedTreeNode && selectedId(this.selectedTreeNode) === p.id;    
-            drawBoundingBox(context, p.boundingBox, tested ? Colors.POLYGON_TESTED : highlighted ? Colors.POLYGON_HIGHLIGHTED : selected ? Colors.POLYGON_SELECTED : Colors.POLYGON);            
-            p.edges.forEach(e => this.drawEdge(context, e, selected));            
+            const selected = isSelectedPolygon(p.id, this.selectedElements);
+            const highlighted = this.selectedTreeNode && selectedId(this.selectedTreeNode) === p.id;
+            drawBoundingBox(context, p.boundingBox, tested ? Colors.POLYGON_TESTED : highlighted ? Colors.POLYGON_HIGHLIGHTED : selected ? Colors.POLYGON_SELECTED : Colors.POLYGON);
+            p.edges.forEach(e => this.drawEdge(context, e, selected));
             p.vertices.forEach(e => this.drawVertex(context, e, selected));
         });
     };
 
     private drawEdge = (context: CanvasRenderingContext2D, edge: IEdge, selected: boolean = false) => {
-        selected = selected || isSelectedEdge(edge.id, this.selectedElements);    
-        const highlighted = this.selectedTreeNode && selectedId(this.selectedTreeNode) === edge.id;    
+        selected = selected || isSelectedEdge(edge.id, this.selectedElements);
+        const highlighted = this.selectedTreeNode && selectedId(this.selectedTreeNode) === edge.id;
         const color = highlighted ? Colors.HIGHLIGHTED : selected ? Colors.EDGE_SELECTED : edge.immaterial ? Colors.IMMATERIAL : Colors.EDGE;
-        const width = selected||highlighted ? 2 : 1;
-        
+        const width = selected || highlighted ? 2 : 1;
+
         if (selected) {
             // draw normal to selected edge:
             drawSegment(context, normal(edge.segment, 10), 'rgb(0,100,255)', 1);
         }
         drawSegment(context, [edge.start.vector, edge.end.vector], color, width);
     };
-       
+
     private drawVertex = (context: CanvasRenderingContext2D, vertex: IVertex, selected: boolean = false) => {
         selected = selected || isSelectedVertex(vertex.id, this.selectedElements);
         const highlighted = this.selectedTreeNode && selectedId(this.selectedTreeNode) === vertex.id;
         drawVector(context, vertex.vector, highlighted ? Colors.HIGHLIGHTED : selected ? Colors.VERTEX_SELECTED : Colors.VERTEX);
-    };    
-    
+    };
+
     private colors: string[] = ['white', 'yellow', 'orange', 'red', 'purple', 'blue', 'cyan', 'green'];
-    private drawBsp = (tree: IBSPNode, depth: number = 0, clipRegion: Path2D[] = []) => {        
-        if (depth > 5) { return; }        
+    private drawBsp = (tree: IBSPNode, depth: number = 0, clipRegion: Path2D[] = []) => {
+        if (depth > 5) { return; }
         if (isSplitNode(tree)) {
-            const [frontClip, backClip] = drawPlane(this._context, tree.plane, this.colors[depth], clipRegion);
-            this.drawBsp(tree.front, depth +1, clipRegion.concat(frontClip));
-            this.drawBsp(tree.back, depth + 1, clipRegion.concat(backClip) );
+            const bounds = this.scroll.toWorldSpace([this.canvas.width, this.canvas.height]);
+            const [frontClip, backClip] = drawPlane(this._context, tree.plane, this.colors[depth], clipRegion, bounds);
+            this.drawBsp(tree.front, depth + 1, clipRegion.concat(frontClip));
+            this.drawBsp(tree.back, depth + 1, clipRegion.concat(backClip));
         }
-    };        
+    };
 }
 
