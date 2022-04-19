ca38ca18bcbfac72323509e5cd44ecdd2d7cd680
Ruben Van Havermaet <ruben.van.havermaet@gmail.com>
2022-04-19 13:51:53 +0200

    introduce heap in zbuffer column

---
+++ src/common/datastructures/heap.ts
@@ -0,0 +1,102 @@
+import { PriorityQueue } from './priority-queue'
+import { swap } from './utils';
+export class Heap<T> implements PriorityQueue<T> {
+  heapSize: number;
+  items: T[];
+  private comparer: (a: T, b: T) => number;
+
+  constructor(items: T[], comparer?: (a: T, b: T) => number) {
+    this.heapSize = items.length;
+    this.comparer = comparer || ((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+    this.items = this.buildHeap(items);
+  }
+
+  insert(element: T): void {
+    let i = this.heapSize++;
+    this.items[i] = element;
+    this.heapFilterUp(i);
+  }
+
+  max = (): T => this.items[0];
+
+  extractMax(): T {
+    const result = this.max();
+    if (result != null) {
+      const last = this.extractLast();
+      if (this.heapSize > 0) {
+        this.items[0] = last;
+        this.heapify(this.items, 0);
+      }
+    }
+    return result;
+  }
+
+  increaseKey(i: number, k: T) {
+    const needIncrease = !this.items[i] || this.comparer(this.items[i], k) < 0;
+    if (needIncrease) {
+      this.items[i] = k;
+      this.heapFilterUp(i);
+    }
+  }
+
+  delete(i: number) {
+    const last = this.extractLast();
+    if (this.heapSize > 0) {
+      this.items[i] = last;
+      if (this.comparer(this.items[i], this.items[parentNode(i)]) >= 1) {
+        this.heapFilterUp(i);
+      } else {
+        this.heapify(this.items, i);
+      }
+    }
+  }
+
+  buildHeap(items: T[]): T[] {
+    return items
+      .filter((_, i) => i <= parentNode(this.heapSize))
+      .reduceRight((a, _y, i) => this.heapify(a, i), items);
+  }
+
+  heapFilterUp(i: number) {
+    while (this.comparer(this.items[i], this.items[parentNode(i)]) >= 1) {
+      swap(this.items, i, parentNode(i));
+      i = parentNode(i);
+    }
+  }
+
+  heapify(a: T[], index: number): T[] {
+    const l = left(index);
+    const r = right(index);
+    let largest = index;
+    if (l < this.heapSize && this.comparer(a[l], a[largest]) >= 1) {
+      largest = l;
+    }
+    if (r < this.heapSize && this.comparer(a[r], a[largest]) >= 1) {
+      largest = r;
+    }
+    if (largest !== index) {
+      return this.heapify(swap(a, index, largest), largest);
+    }
+    return a;
+  }
+
+  extractLast(): T {
+    const result = this.items[--this.heapSize];
+    this.items.length = this.heapSize;
+    return result;
+  }
+
+  static heapsort<T>(items: T[]): T[] {
+    const heap = new Heap<T>(items);
+    while (heap.heapSize > 1) {
+      swap(heap.items, 0, heap.heapSize - 1);
+      heap.heapSize--;
+      heap.heapify(heap.items, 0);
+    }
+    return heap.items;
+  }
+}
+
+export const left = (i: number) => (i + 1) * 2 - 1;
+export const right = (i: number) => (i + 1) * 2;
+export const parentNode = (i: number) =>  Math.max(0, Math.floor((i + 1) / 2) - 1);
--- src/common/rendering/zbuffering/zbuffer-renderer.ts
+++ src/common/rendering/zbuffering/zbuffer-renderer.ts
@@ -23,6 +23,7 @@ import { lineAngle, lineIntersect, segmentLength } from '../../math/lineSegment'
 import { convertDistanceToWallHeight, drawWall, WallProps } from '../common/wall-drawing';
 import { castRaysOnEdge } from '../raycasting/raycaster';
 import { WallPainter } from '../../drawing/wall-painter';
+import { Heap } from '../../datastructures/heap';
 
 const dispatch = useAppDispatch();
 
@@ -68,26 +69,17 @@ export class ZBufferRenderer implements IRenderer {
         // drawSegment(this.context, cone[1].line);
 
         let buffer = new ZBuffer(this.resolution, this.camera, this.wallPainter);
-        let depth = 50;
-        let count = 0;
+
         // TODO: pass direction into walk function so we can use it to ignore entire branch in some cases
         walk(this.wallGeometry.bsp, this.camera.position, (ps => {
             for (let p of ps) {
-                let increment = 0;
                 for (let e of p.edges) {
                     const clipped = this.clip(e);
                     if (clipped === NULL_EDGE) continue;
 
                     buffer.add(clipped);
-                    // const c = 255 - count * 255 / depth;
-                    // drawSegment(this.context, clipped.segment, `rgb(${c},${c},${c})`, 2);
-
-                    //if (buffer.isFull()) return false;
-                    increment = increment || 1;
                 }
-                count = count + increment;
             }
-            //return count <= depth;
             return !buffer.isFull();
         }))
 
@@ -141,83 +133,69 @@ export class ZBuffer {
         let sray = makeRay(this.camera.position, subtract(edge.start.vector, this.camera.position));
         let eray = makeRay(this.camera.position, subtract(edge.end.vector, this.camera.position));
         let sproj = intersectRaySegment(sray, this.camera.screen)?.point;
-        let eproj = intersectRaySegment(eray, this.camera.screen)?.point;
-        
-        if (!sproj || !eproj){return;}
-        // drawSegment(this.context, edge.segment, 'rgb(0,255,0,1)', 1);
-        // drawSegment(this.context, [edge.start.vector, edge.end.vector], 'rgb(0,0,255,1)', 1);
-        // drawSegment(this.context, sray.line, 'rgb(255,255,0,1)', 1);
-        // drawSegment(this.context, eray.line, 'rgb(0, 255,255,1)', 1);
-        // drawSegment(this.context, this.camera.screen, 'rgb(255,255,255,1)', 1);
-        // drawVector(this.context, sproj, 'rgb(255,255,0, 1)');
-        // drawVector(this.context, eproj, 'rgb(0,255,255, 1)');
-
-        
+        let eproj = intersectRaySegment(eray, this.camera.screen)?.point;        
+        if (!sproj || !eproj){ return; }
+                
         let [pl,] = this.camera.screen;
         let screenLength = segmentLength(this.camera.screen);
         let scol = Math.round(distance(pl, sproj)/screenLength * this.resolution);
         let ecol = Math.round(distance(pl, eproj)/screenLength * this.resolution);
         
-        // let sd = distance(edge.start, this.camera.position) * Math.cos(lineAngle(this.camera.midline, sray.line));
-        // let ed = distance(edge.end, this.camera.position) * Math.cos(lineAngle(this.camera.midline, eray.line));
-
-        if (scol < ecol) {            
-            let rays = this.rays.slice(scol, ecol);    
-            let hits = castRaysOnEdge(rays, edge);
-            let i = 0;
-            for (let c of hits) {                                
-                const wall = this.wallPainter.createWall(c, scol+i );
-                
-                this.cols[scol+i].add({isOpaque: (wall.material?.color[3]||0) === 1, render: () => this.wallPainter.drawWall([wall])});
-                this.fullCols++;
-                i++;
-            }            
-        } else {
-            let rays = this.rays.slice(ecol, scol);    
-            let hits = castRaysOnEdge(rays, edge);
-            let i = 0;
-            for (let c of hits) {                                
-                const wall = this.wallPainter.createWall(c, ecol+i );
-                this.cols[ecol+i].add({isOpaque: (wall.material?.color[3]||0) === 1, render: () => this.wallPainter.drawWall([wall])});
-                this.fullCols++;
-                i++;
-            }    
-        }
+        [scol, ecol] = [Math.min(scol,ecol), Math.max(scol, ecol)];        
+        let rays = this.rays.slice(scol, ecol);    
+        let hits = castRaysOnEdge(rays, edge);
+        let i = 0;
+        for (let c of hits) {
+            const wall = this.wallPainter.createWall(c, scol+i );            
+            this.cols[scol+i].add({
+                isOpaque: (wall.material?.color[3]||0) === 1, 
+                distance: c.distance,
+                render: () => this.wallPainter.drawWall([wall])
+            });            
+            i++;
+        }            
     }
-
-    
 
     public isFull(): boolean {   
         return this.cols.every(c => c.isFull());     
-        // return this.fullCols >= this.resolution;
     }
 }
 export class ZBufferColumn {
-    private stack: ZBufferElement[];
+    //private stack: ZBufferElement[];
+    private heap: Heap<ZBufferElement>;
     constructor(private col: number) {
-        this.stack = [];
+        //this.stack = [];
+        this.heap = new Heap<ZBufferElement>([], (a, b) => a.distance - b.distance);
     }
 
     public isFull(): boolean {
-        return this.stack.length > 0 && this.stack[this.stack.length - 1].isOpaque;
+        return this.heap.heapSize > 0 && this.heap.max().isOpaque;
+        //return this.stack.length > 0 && this.stack[this.stack.length - 1].isOpaque;
     }
 
     public add(el: ZBufferElement): ZBufferColumn {
-        if (this.stack.length === 0 || !this.stack[this.stack.length - 1].isOpaque) {
-            this.stack.push(el);
+        if (this.heap.heapSize === 0 || !this.heap.max().isOpaque) {
+            this.heap.insert(el);
         }
+        // if (this.stack.length === 0 || !this.stack[this.stack.length - 1].isOpaque) {
+        //     this.stack.push(el);
+        // }
         return this;
     }
 
     public render(): void {
-        while (this.stack.length > 0) {
-            const el = this.stack.pop();
-            el.render();
+        while (this.heap.heapSize > 0) {
+            this.heap.extractMax().render();
         }
+        // while (this.stack.length > 0) {
+        //     const el = this.stack.pop();
+        //     el.render();
+        // }
     }
 }
 export type ZBufferElement = {
     isOpaque: boolean;
+    distance: number;
     render: () => void;
 }
 
